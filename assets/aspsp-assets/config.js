02893e322f60c997c5cb7db51a7989f23dbc3989
David Monty <david@tell.money>
2020-09-29 16:17:35 +0100

    testing pipeline

--- assets/aspsp-assets/config.js
+++ assets/aspsp-assets/config.js
@@ -1,27 +1,4 @@
-const hostnameArray = window.location.hostname.split('.');
-let subdomain = hostnameArray[0];
-let version = 'v1';
-
-if (subdomain === '') {
-  subdomain = 'aliasdev';
-}
-
-if (subdomain.slice(subdomain.length - 3) === 'dev') version = 'dev';
-
-var get_config = $.ajax({
-  type: "GET",
-  url: "https://system.tell.money/" + version + "/tpp/config/" + subdomain,
-  async: false,
-  headers: {
-    "Content-Type": "application/json"
-  }
-}).done(function (result) {
-});
-const _config = get_config.responseJSON
-
-
-
-/*var _config = {
+var _config = {
   "subdomain": "alias",
   "client_name": "Alias Bank",
   "client_name_lower": "Alias Bank",
@@ -30,4 +7,3 @@ const _config = get_config.responseJSON
   "base_api_path": "https://system.tell.money/dev/",
   "swagger_file": "https://system.tell.money/dev/swagger/interface/example-bank-open-banking-api"
 };
-*/
