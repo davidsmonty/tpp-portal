939e72ae1968c694df0df7708667782db609af26
David Monty <david@tell.money>
2020-09-29 14:59:17 +0100

    testing pipeline

--- assets/aspsp-assets/config.js
+++ assets/aspsp-assets/config.js
@@ -1,14 +1,12 @@
 var _config = {
-    "subdomain": "blocser",
-    "client_name": "Blocser",
-    "client_name_lower": "Blocser",
-    "aisp": true,
-    "pisp": true,
-    "base_api_path": "https://system.tell.money/dev/",
-    "swagger_file": "https://system.tell.money/dev/swagger/interface/blocser",
-    "iface_uuid": "83ceabfb-d07f-4a5e-8bf9-0e8fd74908c3",
-    "iface_name": "Blocser Open Banking API",
-    "aspsp": "Blocser",
-    "aspsp_full_name": "Blocser"
-  };
-
+  "subdomain": "alias",
+  "client_name": "Alias Bank",
+  "client_name_lower": "Alias Bank",
+  "aisp": true,
+  "pisp": true,
+  "base_api_path": "https://system.tell.money/dev/",
+  "swagger_file": "https://system.tell.money/dev/swagger/interface/example-bank-open-banking-api"
+};
+console.log(window.location.hostname);
+const hostnameArray = window.location.hostname.split('.');
+console.log(JSON.stringify(hostnameArray));
