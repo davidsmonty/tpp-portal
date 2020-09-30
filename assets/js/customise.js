2a6495f8b4c654dece7613dd974234be2be070c6
David Monty <david@tell.money>
2020-09-30 13:56:12 +0100

    testing pipeline

--- assets/js/customise.js
+++ assets/js/customise.js
@@ -1,8 +1,3 @@
-console.log('in customise')
-
-
-if (_config.pisp === 0) { console.log('trying to hide')
+if (_config.pisp === 0) {
     $(".pisp").hide();
-} else {console.log('no hide');}
-
-console.log('end customise')
+}
